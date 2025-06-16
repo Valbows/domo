7fa8fb725951e62e87b69cb9685ec0172f003be0
kelveloper <94089030+kelveloper@users.noreply.github.com>
2025-06-15 20:55:05 -0400

    Updated index.js

--- src/backend/api/answer/index.js
+++ src/backend/api/answer/index.js
@@ -1,4 +1,6 @@
 // Answer API endpoint
+
+// Kelvin was here
 export default async function handler(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
